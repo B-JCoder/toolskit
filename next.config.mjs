27e1ade90dcaa59820314212eec3afc03444daea
B-JCoder <bilalcodes777@gmail.com>
2025-12-25 03:29:58 +0500

    remove line

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
-  distDir: 'dist',
+  distDir: "dist",
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -13,6 +12,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
